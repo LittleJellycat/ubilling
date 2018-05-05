7b4a13283126762ed64cc8e91228444d3605ff3b
littlejellycat <littlejellycat@yandex.ru>
2018-05-05 21:35:32 +0300

    Mailer scratch updated

--- src/main/scala/ru/fintech/school/ubilling/mailer/Mail.scala
+++ src/main/scala/ru/fintech/school/ubilling/mailer/Mail.scala
@@ -1,16 +1,18 @@
 package ru.fintech.school.ubilling.mailer
 
-case class Mail(from: String,
-                to: Seq[String],
+case class Mail(to: String,
                 subject: String,
-                message: String) {
-  def createMail(positions: Seq[String], total: Double, link: String): String = {
-    s"""
-       |<ol>
-       |${positions.map(p => """<li>$p</li>""")}
-       |</ol>
-       |Total: $total
-       |Pay here: $link
-      """.stripMargin
+                message: String)
+
+object Mail {
+  def createTotalMail(positions: Seq[String], total: Double, link: String)(to: String, subject: String): Mail = {
+    Mail(to, subject,
+      s"""
+         |<ol>
+         |${positions.map(p => """<li>$p</li>""")}
+         |</ol>
+         |Total: $total
+         |Pay here: $link
+      """.stripMargin)
   }
 }
--- src/main/scala/ru/fintech/school/ubilling/mailer/Mailer.scala
+++ src/main/scala/ru/fintech/school/ubilling/mailer/Mailer.scala
@@ -1,14 +1,17 @@
 package ru.fintech.school.ubilling.mailer
 
-import java.io.FileInputStream
 import java.util.Properties
 import javax.mail.internet.{InternetAddress, MimeMessage}
 import javax.mail.{Message, Session, Transport}
 
+import ru.fintech.school.ubilling.BillApp
+
 object Mailer {
   val session: Session = {
     val initProperties = new Properties()
-    initProperties.load(new FileInputStream("ru/fintech/school/ubilling/mailer/credentials.properties"))
+    val stream = Mailer.getClass.getResourceAsStream("/credentials.properties")
+    println(stream)
+    initProperties.load(stream)
     val host = initProperties.getProperty("login")
     val login = initProperties.getProperty("host")
     val password = initProperties.getProperty("password")
@@ -17,20 +20,19 @@ object Mailer {
     properties.setProperty("mail.user", login)
     properties.setProperty("mail.password", password)
     Session.getDefaultInstance(properties)
-
   }
 
   def sendMail(mail: Mail): Unit = {
     try {
       val message = new MimeMessage(session)
-      message.setFrom(mail.from)
-      mail.to.foreach(to => message.addRecipient(Message.RecipientType.TO, new InternetAddress(to)))
+      message.setFrom(session.getProperty("mail.user"))
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress(mail.to))
       message.setSubject(mail.subject)
       message.setContent(mail.message, "text/html")
       Transport.send(message)
     } catch {
       case e: Exception =>
-        e.printStackTrace()
+        BillApp.logger.error(e.getMessage)
     }
   }
 }
